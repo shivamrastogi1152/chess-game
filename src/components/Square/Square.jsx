0f37316282d73e23b23f1a2c9607e5229fc9d005
Shivam Rastogi <shivamrastogi7890@gmail.com>
2024-03-27 08:49:32 +0530

    Improve attack preview moves

--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -3,9 +3,9 @@ import { HORIZONTAL_AXIS, VERTICAL_AXIS } from '../../utils/utils';
 
 function Square({row, col, imgSrc, highlight}){
     const className = 'square ' + 
-    (((row + col) % 2 === 1) ? ' light-square ' : ' dark-square ') + 
-    ((highlight ? ' highlight ' : '')) + 
-    ((imgSrc!=null ? 'chess-piece-square' : ''));
+    (((row + col) % 2 === 1) ? 'light-square ' : 'dark-square ') + 
+    ((highlight ? 'highlight ' : '')) + 
+    ((imgSrc!=null ? 'chess-piece-square ' : ''));
     return (
     <div className={className}>
         {imgSrc!=null ? <div style={{backgroundImage : `url(${imgSrc})`}} className='chess-piece'/> : <></>}
